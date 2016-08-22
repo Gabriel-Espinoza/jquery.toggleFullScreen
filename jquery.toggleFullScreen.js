edefcdf19560113784ab76d3bcbd9b60c351650e
Gabriel Espinoza <gespinoza@ad.pricing.cl>
2016-08-22 16:06:58 -0300

    bug fixes of first commit. This version is fully functional

--- jquery.toggleFullScreen.js
+++ jquery.toggleFullScreen.js
@@ -1,5 +1,5 @@
 (function ($) {
-    $.fn.toggleFullScreen = function(options, callback) {
+    $.fn.toggleFullScreen = function(cssOptions, callback) {
         
         var settings = $.extend({
             width: "100%",
@@ -8,39 +8,75 @@
             left: 0,
             position: "fixed",
             display: "block",
-            zindex: 999999
-        }, options );
+            zindex: 999999,
+            backgroundcolor: "white",
+            class: null
+        }, cssOptions );
 
-        var changedToFullScreen = false;
+        if(typeof this.data("toggleFullScreen-isFullScreen") === "undefined")
+            this.data("toggleFullScreen-isFullScreen", false);
+        var isFullScreen = this.data("toggleFullScreen-isFullScreen");
 
-        if(this.data("toggleFullScreen-isFullScreen") === false) {
-            
-        }
-        
-
-        if (this.hasClass(className)) {
+        if (isFullScreen) 
+        {
             var parentObject = this.data("toggleFullScreen-currentParent");
             this.appendTo(parentObject);
-            this.removeClass(className);
-        } else {
+
+            if(settings.class) {
+                this.removeClass(settings.class);
+            }
+            else {
+                var originalCssStyle = this.data("toggleFullScreen-currentCssStyle");
+                setCss(this, originalCssStyle);
+            }
+        } 
+        else 
+        {
             var parentObject = this.parent();
             this.data("toggleFullScreen-currentParent", parentObject);
             this.appendTo("body");
-            this.addClass(className);
+
+            if(settings.class) {
+                this.addClass(settings.class);
+            }
+            else {
+                var originalCssStyle = getCurrentCss(this);
+                this.data("toggleFullScreen-currentCssStyle", originalCssStyle);
+                setCss(this, settings);
+            }
         }
 
-         if ($.isFunction(callback))
-                callback(this, this, changedToFullScreen);
+        this.data("toggleFullScreen-isFullScreen", !isFullScreen);
+        if ($.isFunction(callback))
+            callback(this, !isFullScreen);
         
         return this;
     };
 
-    // $.fn.toggleFullScreenActivator = function()
-    // {
-    //     var settings = $.extend({
-    //         target_div: this.attr("data-toggleFullScreen-div")
-    //     }, options );
-    // }
+   function setCss(domElement, rulesToApply) {
+        domElement.css("width", rulesToApply.width)
+            .css("height", rulesToApply.height)
+            .css("top", rulesToApply.top)
+            .css("left", rulesToApply.left)
+            .css("position", rulesToApply.position)
+            .css("display", rulesToApply.display)
+            .css("background-color", rulesToApply.backgroundcolor)
+            .css("z-index", rulesToApply.zindex);
+   }
+
+   function getCurrentCss(domElement) {
+        var originalCssStyle = {
+            width: domElement.css("width"),
+            height: domElement.css("height"),
+            top: domElement.css("top"),
+            left: domElement.css("left"),
+            position: domElement.css("position"),
+            display: domElement.css("display"),
+            backgroundcolor: domElement.css("background-color"),
+            zindex: domElement.css("zindex"),
+        }
+        return originalCssStyle;
+   }
 
 }(jQuery));
 
